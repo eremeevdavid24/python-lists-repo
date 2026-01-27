65f78974d96dc00393e21a1bbef9811ba436660e
Eremeev David <eredamdavmail.ru@gmail.com>
2026-01-27 22:14:36 +0200

    Add files remove_elements.py

---
+++ remove_elements.py
@@ -0,0 +1,16 @@
+numbers = [10, 20, 30, 40]
+
+# delete by value
+#numbers.remove(20)
+#print(numbers)
+
+
+# dlete by index
+#removed_element = numbers.pop(1)
+#print(numbers)
+#print(removed_element)
+
+
+# delete entire list
+numbers.clear()
+print(numbers)
