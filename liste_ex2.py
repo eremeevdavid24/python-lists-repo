90c6976fa5d8bbbf8c90b7b9c30a3069fd105f33
Eremeev David <eredamdavmail.ru@gmail.com>
2026-01-27 21:32:06 +0200

    Add files

---
+++ liste_ex2.py
@@ -0,0 +1,8 @@
+numbers =[10, 20, 30, 40]
+
+for number in numbers:
+    print(number)
+
+statele_ale_lumii = ['Franta', 'Italia', 'Belgia']
+print(statele_ale_lumii[-3])
+
--- liste_primul_exemplu.py
+++ liste_primul_exemplu.py
@@ -1,1 +1,23 @@
-print("Liste")
+numbers =[10, 20, 30, 40]
+print(numbers)
+
+print(numbers[3]) # 40
+
+numbers[2] = 300
+
+print(numbers)
+
+# add elements
+numbers.append(100)
+numbers.append( 200)
+print(numbers)
+
+#remove elements
+numbers.pop(1)
+numbers.remove(10)
+print(numbers)
+
+# cler elements
+numbers.clear()
+print(numbers)
+
