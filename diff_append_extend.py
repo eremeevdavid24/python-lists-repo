02f97ad92ea1322f00c895af405c29088eb178c2
Eremeev David <eredamdavmail.ru@gmail.com>
2026-01-27 21:55:44 +0200

    Add files diff

---
+++ diff_append_extend.py
@@ -0,0 +1,11 @@
+# append
+numbers = [1, 2, 3]
+
+numbers.append([4,5])
+print(numbers)
+
+#extend
+numbers = [1, 2, 3]
+
+numbers.extend([4, 5])
+print(numbers)
---
+++ liste_ex3.py
@@ -0,0 +1,8 @@
+statele_ale_lumii = ['Franta', 'Italia', 'Belgia']
+
+state_noi = ['Spania', 'Germania']
+statele_ale_lumii.extend(state_noi)
+
+statele_ale_lumii.append(state_noi)
+
+print(statele_ale_lumii)
